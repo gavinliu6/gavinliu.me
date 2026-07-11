b441c718929d3f41cbf4db853e11fd8a0f4d7444
Gavin Liu <29831211+gavinliu6@users.noreply.github.com>
2026-07-11 23:43:53 +0800

    feat: add skip to content link

--- .vitepress/theme/components/layouts/Layout.vue
+++ .vitepress/theme/components/layouts/Layout.vue
@@ -13,12 +13,36 @@ const { frontmatter } = useData()
 </script>
 
 <template>
-  <HomePage v-if="frontmatter.home" />
+  <a
+    href="#main-content"
+    class="
+      pointer-events-none fixed top-16 left-12 z-1000 -m-px overflow-hidden
+      rounded-md border-0 bg-background-100 px-1 py-0 text-label-14
+      whitespace-nowrap text-blue-700 opacity-0
+      shadow-[0_0_0_2px_var(--ds-background-100),0_0_0_4px_var(--ds-blue-700)]
+      outline-none
+      focus:pointer-events-auto focus:opacity-100
+    "
+  >
+    Skip to content
+  </a>
+  <main
+    v-if="frontmatter.home"
+    id="main-content"
+    tabindex="-1"
+    class="outline-none"
+  >
+    <HomePage />
+  </main>
   <template v-else>
     <TooltipProvider>
       <div class="flex min-h-screen flex-col">
         <AppHeader />
-        <main>
+        <main
+          id="main-content"
+          tabindex="-1"
+          class="outline-none"
+        >
           <BlogPage v-if="frontmatter.blog" />
           <PostDetailPage v-else />
         </main>
